573f0998c8e99d5dc25ec9f744413d2318fae25b
Deniss Dmitrijevs 221RDB322 <125139518+deniss-dmitrijevs@users.noreply.github.com>
2023-04-15 21:52:29 +0000

    implemented

--- hash_substring.py
+++ hash_substring.py
@@ -4,6 +4,19 @@ def read_input():
     # this function needs to aquire input both from keyboard and file
     # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
     
+    in_type = input()
+
+    if "i" in in_type.lower():
+        pattern = input()
+        text = input()
+    elif "f" in in_type.lower():
+        file_name = str(input())
+        path = "tests/" + file_name
+        with open(path, 'r') as file:
+            pattern = file.readline()
+            text = file.readline()
+
+
     
     # after input type choice
     # read two lines 
@@ -13,7 +26,7 @@ def read_input():
     # return both lines in one return
     
     # this is the sample return, notice the rstrip function
-    return (input().rstrip(), input().rstrip())
+    return (pattern.rstrip(), text.rstrip())
 
 def print_occurrences(output):
     # this function should control output, it doesn't need any return
@@ -21,10 +34,22 @@ def print_occurrences(output):
 
 def get_occurrences(pattern, text):
     # this function should find the occurances using Rabin Karp alghoritm 
-
+    instance_indices = []
+    pattern_length = len(pattern)
+    pattern_hash = get_text_hash(pattern)
     # and return an iterable variable
-    return [0]
+    for i in range(0, len(text), pattern_length):
+        if get_text_hash(text[i:i+pattern_length]) == pattern_hash:
+            instance_indices.append(i)
+    return instance_indices
 
+def get_text_hash(text: str) -> int:
+    multiplier = 23
+    modulo = 137
+    hash = 0
+    for char in text:
+        hash = (hash * multiplier + ord(char)) % modulo
+    return hash
 
 # this part launches the functions
 if __name__ == '__main__':
